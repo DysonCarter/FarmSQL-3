459677ddbc622233149bb6dfc2d31bbf4fd2e8f6
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-03-19 11:00:25 -0400

    Fix typo

--- farm/src/components/InstructionText.js
+++ farm/src/components/InstructionText.js
@@ -11,7 +11,7 @@ const InstructionText = ({ level, onLevelChange }) => {
         return (
           <div>
             <span style={{ color: textGreen }}>Well Done!</span><br/><br/>
-            The keyword <span style={{ color: textGreen }}>SELECT</span> is what you use to pick out data from your database, and <span style={{ color: textGreen }}>FROM farm</span> specificies you're choosing data from the database "<span style={{ color: textGreen }}>farm</span>" <br/><br/>
+            The keyword <span style={{ color: textGreen }}>SELECT</span> is what you use to pick out data from your database, and <span style={{ color: textGreen }}>FROM farm</span> specifies you're choosing data from the database "<span style={{ color: textGreen }}>farm</span>" <br/><br/>
             In SQL the star "<span style={{ color: textGreen }}>*</span>" represents all attributes,<br/><br/>
             You can also pick specific attributes. Show me all of the names in my farm, and <span style={{ color: 'red' }}>ONLY</span> the names! <br/><br/>
             <span style={{ color: "rgb(50,50,50)"}}>SELECT (Which Attribute?) FROM farm</span>
